0ec22cace88eceee0678d31db69517ea40feb8d0
ironsummit72 <ironsumit72@gmail.com>
2023-10-30 11:45:14 +0530

    changed the port to 5001

--- server.js
+++ server.js
@@ -32,6 +32,6 @@ if (process.env.NODE_ENV === 'production') {
   })
 }
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5001
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
